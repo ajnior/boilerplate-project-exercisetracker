4c0eab671f9ace5a619af82ec1a98c8efd773244
Agnaldo Cardoso Junior <acjunior.k2@rd.com.br>
2020-12-11 14:31:42 -0300

    feat: add ref to Log into user model

--- models/user.js
+++ models/user.js
@@ -2,7 +2,7 @@ const { Schema, model } = require("mongoose");
 
 const userSchema = new Schema({
   username: { type: String, required: [true, "username is required"] },
-  log: { type: Schema.Types.ObjectId },
+  log: { type: Schema.Types.ObjectId, ref: "Log" },
 });
 
 const User = model("User", userSchema);
